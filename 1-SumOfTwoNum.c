2ad1f6bb7d4bc11c98c6099d27f9d533bf4fb4a0
Harishkumar-jk <78493541+Harishkumar-jk@users.noreply.github.com>
2021-02-19 00:33:15 +0530

    Add files via upload

---
+++ 1-SumOfTwoNum.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    long long a, b ; // double lf
+    printf("Enter two nums : ");
+    scanf("%lld%lld",&a,&b);
+    printf("Their sum : %lld",a+b);
+
+
+    return 0;
+}
